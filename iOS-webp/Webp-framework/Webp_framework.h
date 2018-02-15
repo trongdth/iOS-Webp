8ef7a8101ff63ca7b8ac8152542c19b83d4f9d4d
Trong Dinh <trongdth@gmail.com>
2018-02-15 14:13:02 +0700

    Update framework header

--- iOS-webp/Webp-framework/Webp_framework.h
+++ iOS-webp/Webp-framework/Webp_framework.h
@@ -15,5 +15,6 @@ FOUNDATION_EXPORT double Webp_frameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char Webp_frameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Webp_framework/PublicHeader.h>
+#import <Webp_framework/UIImage+WebP.h>
 
 
